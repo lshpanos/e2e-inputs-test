0c1fdeb12f32e715555cbdec8df5ee7464283e12
Panagiotis Angelidakis <panagiotisa@lsnova.gr>
2018-08-17 14:37:35 +0300

    Some basic tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,30 @@
-import { AppPage } from './app.po';
+import {AppPage} from './app.po';
 
-describe('workspace-project App', () => {
+describe('A simple App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to e2e-inputs-test!');
+  it('First name should be Nick', () => {
+    expect(page.getFirstNameText()).toEqual('');
+  });
+
+  it('Last name should be Protractor', () => {
+    expect(page.getLastNameText()).toEqual('Protractor');
+  });
+
+  it('Address input should be disabled', () => {
+    expect(page.getAddressStatus()).toBe('true');
+  });
+
+  it('Check box should be checked', () => {
+    expect(page.getCheckBoxSelection()).toBe(true);
+  });
+
+  it('List selection value should be Fair', () => {
+    expect(page.getListSelection()).toEqual('Fair');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,27 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element} from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getFirstNameText() {
+    return element(by.id('first-name')).getText();
+  }
+
+  getLastNameText() {
+    return element(by.className('last-name')).getText();
+  }
+
+  getAddressStatus() {
+    return element(by.id('address')).getAttribute('disabled');
+  }
+
+  getCheckBoxSelection() {
+    return element(by.id('isChecked')).isSelected();
+  }
+
+  getListSelection() {
+    return element(by.id('aList')).getAttribute('value');
   }
 }
