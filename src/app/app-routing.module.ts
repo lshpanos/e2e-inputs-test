52b0ee8dba38ee07a9e2e9ef16c1b2cbcaca39f0
Panagiotis Angelidakis <panagiotisa@lsnova.gr>
2018-08-21 09:04:09 +0300

    Populate blog component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CalculatorComponent} from './calculator/calculator.component';
 import {BasicInputElementsComponent} from './basic-input-elements/basic-input-elements.component';
+import {BlogComponent} from './blog/blog.component';
 
 const routes: Routes = [
   {path: 'basic-input-elements', component: BasicInputElementsComponent},
-  {path: 'calculator', component: CalculatorComponent}
+  {path: 'calculator', component: CalculatorComponent},
+  {path: 'blog', component: BlogComponent}
 ];
 
 @NgModule({
--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  title = 'Blog Component';
+  post = {title: '', body: ''};
+  posts = [];
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  addPost() {
+    console.log(this.post);
   }
 
 }
